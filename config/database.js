7d28d86af0b08da52a2afcc5741bfbc268389ce5
정인호 <wjddlsgh0203@gmail.com>
2024-09-27 22:59:08 +0900

    database.js 수정

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
-const config = require('./config.json');
+// const config = require('./config.json');
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
+// const env = process.env.NODE_ENV || 'development';
+// const dbConfig = config[env];
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    port: dbConfig.port,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
     dialectOptions: {
-        "connectTimeout": 60000
+        connectTimeout: 60000
     }
 });
 
