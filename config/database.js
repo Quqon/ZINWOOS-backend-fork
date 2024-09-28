6eddddd3b7b65ff2934fd4ba4b4409ed66692f91
정인호 <wjddlsgh0203@gmail.com>
2024-09-28 13:31:58 +0900

    port 변경2

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3000,
+    port: 30781,
     dialectOptions: {
         connectTimeout: 60000
     }
